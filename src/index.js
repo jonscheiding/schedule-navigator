e04fd9f804d29055227aba8984d7da41680915c5
Jonathan Scheiding <jscheiding@certicasolutions.com>
2018-10-11 14:56:56 -0400

    Hot reload, eslint, editorconfig

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App';
+
+const render = (AppComponent) => {
+  ReactDOM.render(
+    <AppComponent />,
+    document.getElementById('root')
+  );
+};
+
+render(App);
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NewApp = require('./App').default;
+    render(NewApp);
+  });
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
