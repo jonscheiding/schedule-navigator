73b03e3790dfb3133995524dc0452c07e4f349e1
Jonathan Scheiding <jscheiding@certicasolutions.com>
2018-10-15 15:30:02 -0400

    Enabled Redux DevTools

--- src/state/create-store.js
+++ src/state/create-store.js
@@ -4,5 +4,8 @@ import getInitialState from './get-initial-state';
 import app from './reducers';
 
 export default function() {
-  return createStore(app, getInitialState());
+  return createStore(
+    app, 
+    getInitialState(),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 }
